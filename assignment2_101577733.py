a3111e498f81bf78e230d2c51dd90093e7d743f3
jkenny264 <johnkenny264@gmail.com>
2026-03-30 23:13:23 -0400

    completed unit tests

--- assignment2_101577733.py
+++ assignment2_101577733.py
@@ -1,33 +1,63 @@
 """
-Author: <YOUR REAL FIRST AND LAST NAME>
+Author: John Kenny
 Assignment: #2
 Description: Port Scanner — A tool that scans a target machine for open network ports
 """
 
-# TODO: Import the required modules (Step ii)
-# socket, threading, sqlite3, os, platform, datetime
+import socket
+import threading
+import sqlite3
+import os
+import platform
+import datetime
 
 
-# TODO: Print Python version and OS name (Step iii)
+print("Python Version: ", platform.python_version())
+print("Operating System: ", os.name)
 
 
-# TODO: Create the common_ports dictionary (Step iv)
-# Add a 1-line comment above it explaining what it stores
+
+# this dictionary stores commonly used port numbers with their corresponding service
+common_ports = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3306: "MySQL",
+    3389: "RDP",
+    8080: "HTTP-Alt"
+}
+
+class NetworkTool:
+    def __init__(self, target: str):
+        self.__target = target
+
+    @property
+    def target(self):
+        return self.__target
+    
+    @target.setter
+    def target(self):
+        if self.target != "":
+            return self.__target
+        else:
+            print("Error: Target cannot be empty")
+
+    def __del__(self):
+        print("NetworkTool instance destroyed")
 
 
-# TODO: Create the NetworkTool parent class (Step v)
-# - Constructor: takes target, stores as private self.__target
-# - @property getter for target
-# - @target.setter with empty string validation
-# - Destructor: prints "NetworkTool instance destroyed"
 
 
 # Q3: What is the benefit of using @property and @target.setter?
 # TODO: Your 2-4 sentence answer here... (Part 2, Q3)
 
 
-# Q1: How does PortScanner reuse code from NetworkTool?
-# TODO: Your 2-4 sentence answer here... (Part 2, Q1)
 
 # TODO: Create the PortScanner child class that inherits from NetworkTool (Step vi)
 # - Constructor: call super().__init__(target), initialize self.scan_results = [], self.lock = threading.Lock()
@@ -58,12 +88,83 @@ Description: Port Scanner — A tool that scans a target machine for open networ
 #     - Join all threads (separate loop)
 
 
+
+"""
+Portscanner reuses code from NetworkTool through inheritance. 
+One example of how it does this is through its setter function for the target field.
+The setter function for the target field is: target(). 
+"""
+class PortScanner(NetworkTool):
+    def __init__(self, target = ''):
+        super().__init__(target)
+        self.scan_results = []
+        self.lock = threading.Lock()
+    
+    def __del__(self):
+        super().__del__()
+        print("PortScanner instance destroyed")
+
+    def scan_port(self, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((self.target, port))
+            if result == 0:
+                status = "Open"
+            else:
+                status = "Closed"
+                   
+            if port not in common_ports.keys():
+                service_name = "Unknown"
+            else:
+                service_name = common_ports[port]
+            self.lock.acquire()
+            self.scan_results.append((port, status, service_name))
+            self.lock.release()
+        except socket.error as ex:
+            print(f"Error scanning port {port}: {ex}")
+        finally:
+            sock.close()
+
+    def get_open_ports(self):
+        return [x for x in self.scan_results if x[1] == "Open"]
+    
+    def scan_range(self, start_port, end_port):
+        threads = []
+        ports = self.get_open_ports()
+        for port in ports:
+            thread = threading.Thread(target=self.scan_port, args=(port,))
+            threads.append(thread)
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+
+
 # TODO: Create save_results(target, results) function (Step vii)
 # - Connect to scan_history.db
 # - CREATE TABLE IF NOT EXISTS scans (id, target, port, status, service, scan_date)
 # - INSERT each result with datetime.datetime.now()
 # - Commit, close
 # - Wrap in try-except for sqlite3.Error
+
+def save_results(target, results):
+    conn = sqlite3.connect("scan_history.db")
+    cur = conn.cursor()
+
+    if not os.path.isfile("scan_history.db"):
+        cur.execute('''
+            CREATE TABLE scans (
+            id INTEGER PRIMARY KEY AUTOINCREMENT ,
+            target TEXT,
+            port INTEGER,
+            status TEXT,
+            service TEXT,
+            scan_date TEXT
+            )
+        ''')
+
 
 
 # TODO: Create load_past_scans() function (Step viii)
@@ -85,6 +186,12 @@ if __name__ == "__main__":
     # - End port (1-1024, >= start port)
     # - Catch ValueError: "Invalid input. Please enter a valid integer."
     # - Range check: "Port must be between 1 and 1024."
+    try:  
+        target = input("enter a target IP address: (default is 127.0.0.1)") or "127.0.0.1"
+        start_port = input("enter a start port between 1 and 1024:")
+        end_port = input("enter a start port between 1 and 1024:")
+    except ValueError as ex: 
+        "Invalid input. Please enter a valid integer."
 
     # TODO: After valid input (Step x)
     # - Create PortScanner object
@@ -95,6 +202,17 @@ if __name__ == "__main__":
     # - Call save_results()
     # - Ask "Would you like to see past scan history? (yes/no): "
     # - If "yes", call load_past_scans()
+
+    ps1 = PortScanner(target)
+
+    print(f"Scanning {target} from port {start_port} to {end_port}...")
+    ps1.scan_range(start_port, end_port)
+    open_ports = ps1.get_open_ports()
+
+    print(f"--- Scan Results for {target} ---")
+    for port in open_ports:
+        print(f"Port: {port.port}: {port.status} ({port.service_name})")
+    save_results(target, open_ports)
 
 
 # Q5: New Feature Proposal
